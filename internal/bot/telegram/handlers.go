2b1d9f89741bcf3010c1623de64c3a8c27cf87d6
VPG1 <malihtorovich@mail.ru>
2024-12-20 18:58:35 +0300

    Add tracking and list handlers

--- internal/bot/telegram/handlers.go
+++ internal/bot/telegram/handlers.go
@@ -1,15 +1,20 @@
 package telegram
 
 import (
+	"AirLineTicketTracker/internal/services"
+	"errors"
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 	"strconv"
+	"strings"
 )
 
 const (
 	commandStart = "start"
 	commandHelp  = "help"
 	commandTrack = "track"
+	commandList  = "list"
 	commandStop  = "stop"
 )
 
@@ -19,9 +24,15 @@ func (b *Bot) handleCommand(message *tgbotapi.Message) error {
 		return b.handleStartCommand(message)
 	case commandHelp:
 		return b.handleHelpCommand(message)
+	case commandTrack:
+		return b.handleTrackCommand(message)
+	case commandList:
+		return b.handleListCommand(message)
 	default:
 		return nil
 	}
+
+	return nil
 }
 
 func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
@@ -49,9 +60,87 @@ func (b *Bot) handleHelpCommand(message *tgbotapi.Message) error {
 	return nil
 }
 
-//func (b *Bot) handleStopCommand(message *tgbotapi.Message) error {
-//	msg
-//}
+func (b *Bot) handleTrackCommand(message *tgbotapi.Message) error {
+	text := message.Text
+	text = strings.TrimPrefix(text, "/track") // удаляем префикс сообщения
+
+	flight, err := b.trackingService.TrackFlight(message.Chat.ID, text)
+	if errors.Is(err, services.IncorrectSearchPhrase) { // некорректная поисковая фраза
+		msg := tgbotapi.NewMessage(message.Chat.ID, incorrectSearchPhraseResponse)
+		_, err = b.tgBotApi.Send(msg)
+		if err != nil {
+			return err
+		}
+		return nil
+	} else if errors.Is(err, services.UserNotRegistered) { // пользователь не вводил команду старт
+		msg := tgbotapi.NewMessage(message.Chat.ID, userNotInSystem)
+
+		_, err = b.tgBotApi.Send(msg)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	} else if errors.Is(err, services.FlightAlreadyTracked) {
+		formatedDate := flight.DepartureAt.Format("January 2, 2006 15:04 Monday")
+
+		msg := tgbotapi.NewMessage(message.Chat.ID,
+			fmt.Sprintf(userAlreadyTrackFlight, flight.Origin, flight.Destination, formatedDate, flight.Price))
+
+		_, err = b.tgBotApi.Send(msg)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	formatedDate := flight.DepartureAt.Format("January 2, 2006 15:04 Monday")
+
+	msg := tgbotapi.NewMessage(message.Chat.ID,
+		fmt.Sprintf(trackResponse, flight.Origin, flight.Destination, formatedDate, flight.Price))
+
+	_, err = b.tgBotApi.Send(msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *Bot) handleListCommand(message *tgbotapi.Message) error {
+	flights := b.trackingService.GetUserFlight(message.Chat.ID)
+
+	if len(flights) == 0 {
+		msg := tgbotapi.NewMessage(message.Chat.ID, NoTrackedFlights)
+
+		_, err := b.tgBotApi.Send(msg)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	flightsString := ""
+	for i, flight := range flights {
+		flightsString += "✈️ " + strconv.Itoa(i+1) + ". " +
+			flight.Origin + " → " + flight.Destination +
+			" (Текущая цена: " + strconv.Itoa(flight.Price) + "$)" + "\n"
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID,
+		fmt.Sprintf(listResponse, flightsString))
+
+	_, err := b.tgBotApi.Send(msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (b *Bot) handleMessage(message *tgbotapi.Message) error {
 	log.Println(strconv.Itoa(int(message.Chat.ID)) +
